1cc02b354b72c0e77f7fc770b2e5ade058427515
Micha Niskin <micha.niskin@gmail.com>
2014-11-15 18:27:55 -0500

    0.1.6 -- Fix wrong arity of noop used when no on-jsload option

--- build.boot
+++ build.boot
@@ -5,7 +5,7 @@
 
 (require '[tailrecursion.boot-useful :refer :all])
 
-(def +version+ "0.1.5")
+(def +version+ "0.1.6")
 
 (useful! +version+)
 
--- src/adzerk/boot_reload.clj
+++ src/adzerk/boot_reload.clj
@@ -39,7 +39,7 @@
              ~@(when on-jsload [(symbol (namespace on-jsload))])))
           (when-not (client/alive?)
             (client/connect ~url
-              {:on-jsload #(~(or on-jsload 'identity))}))))
+              {:on-jsload #(~(or on-jsload '+))}))))
     (map pr-str) (interpose "\n") (apply str) (spit f)))
 
 (defn- send-changed! [pod changed]
