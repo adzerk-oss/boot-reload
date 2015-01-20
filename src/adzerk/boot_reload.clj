bd6d34fab98cd1af203ed73df447629cadca43cf
Alexander Solovyov <alexander@solovyov.net>
2015-01-20 12:56:32 +0200

    skip nodejs targets

--- src/adzerk/boot_reload.clj
+++ src/adzerk/boot_reload.clj
@@ -46,14 +46,15 @@
 
 (defn- add-init!
   [in-file out-file]
-  (let [ns 'adzerk.boot-reload]
-    (info "Adding :require %s to %s...\n" ns (.getName in-file))
-    (-> in-file
-        slurp
-        read-string
+  (let [ns 'adzerk.boot-reload
+        spec (-> in-file slurp read-string)]
+    (when (not= :nodejs (-> spec :compiler-options :target))
+      (info "Adding :require %s to %s...\n" ns (.getName in-file))
+      (io/make-parents out-file)
+      (-> spec
         (update-in [:require] conj ns)
         pr-str
-        ((partial spit out-file)))))
+        ((partial spit out-file))))))
 
 (deftask reload
   "Live reload of page resources in browser via websocket.
