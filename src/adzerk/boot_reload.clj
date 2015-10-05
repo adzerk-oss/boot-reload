50ff3f2ef97f7a960b894ed6211cce3324ab2a6c
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-10-05 21:06:50 +0300

    Introduce the Figwheel client

--- src/adzerk/boot_reload.clj
+++ src/adzerk/boot_reload.clj
@@ -7,12 +7,13 @@
    [clojure.string     :as string]
    [boot.pod           :as pod]
    [boot.file          :as file]
-   [boot.util          :as util]
+   [boot.util          :as butil]
    [boot.core          :refer :all]
-   [boot.from.backtick :as bt]
-   [adzerk.boot-reload.util :as rutil]))
+   [boot.from.digest        :as digest]
+   [adzerk.boot-reload.util :as util]))
 
-(def ^:private deps '[[http-kit "2.1.18"]])
+(def ^:private deps '[[http-kit "2.1.18"]
+                      [figwheel-sidecar "0.5.9-SNAPSHOT"]])
 
 (defn- make-pod []
   (future (-> (get-env) (update-in [:dependencies] into deps) pod/make-pod)))
@@ -26,67 +27,86 @@
       (->> (fileset-diff before after :hash)
            output-files
            maybe-filter-by-re
+           (not-by-path static-files)
            (sort-by :dependency-order)
-           (map tmp-path)
-           (remove static-files)))))
+           (reduce #(conj %1 {:relative-path (-> %2 tmp-path)
+                              :dir-path (-> %2 tmp-dir .getCanonicalPath)
+                              :full-path (-> %2 tmp-file .getCanonicalPath)})
+                   #{})))))
 
 (defn- start-server [pod {:keys [ip port ws-host ws-port secure?] :as opts}]
   (let [{:keys [ip port]} (pod/with-call-in pod (adzerk.boot-reload.server/start ~opts))
         listen-host       (cond (= ip "0.0.0.0") "localhost" :else ip)
         client-host       (cond ws-host ws-host (= ip "0.0.0.0") "localhost" :else ip)
         proto             (if secure? "wss" "ws")]
-    (util/info "Starting reload server on %s\n" (format "%s://%s:%d" proto listen-host port))
+    (butil/info "Starting reload server on %s\n" (format "%s://%s:%d" proto listen-host port))
     (format "%s://%s:%d" proto client-host (or ws-port port))))
 
-(defn- write-cljs! [tmp client-ns url ws-host on-jsload asset-host]
-  (util/info "Writing %s to connect to %s...\n" (.getPath (rutil/ns->file client-ns "cljs")) url)
-  (let [out (doto (rutil/ns->file tmp client-ns "cljs")
-              io/make-parents)]
-    (->> (bt/template
-          ((ns ~(symbol client-ns)
-             (:require
-              [adzerk.boot-reload.client :as client]
-              ~@(when on-jsload [(symbol (namespace on-jsload))])))
-           (client/connect ~url {:on-jsload #(~(or on-jsload '+))
-                                 :asset-host ~asset-host
-                                 :ws-host ~ws-host})))
-         (map pr-str)
-         (interpose "\n")
-         (apply str)
-         (spit out))))
+(defn- write-bootstrap-ns! [pod parent-path build-config]
+  (let [[ns-sym ns-path ns-content] (pod/with-call-in pod
+                                      (adzerk.boot-reload.server/bootstrap-ns
+                                       ~build-config))
+        f (io/file parent-path ns-path)]
+    (io/make-parents f)
+    (butil/info "Writing %s namespace to %s...\n" ns-sym (.getName f))
+    (butil/dbug "%s\n" ns-content)
+    (spit f ns-content)))
 
-(defn- send-visual! [pod messages]
-  (when-not (empty? messages)
-    (pod/with-call-in pod
-      (adzerk.boot-reload.server/send-visual!
-        ~messages))))
+(defn- send-visual! [pod client-opts visual-map]
+  (pod/with-call-in pod
+    (adzerk.boot-reload.server/send-visual!
+     ~client-opts
+     ~visual-map)))
 
-(defn- send-changed! [pod asset-path cljs-asset-path target-path changed]
-  (when-not (empty? changed)
-    (pod/with-call-in pod
-      (adzerk.boot-reload.server/send-changed!
-       {:target-path ~target-path
-        :asset-path ~asset-path
-        :cljs-asset-path ~cljs-asset-path}
-        ~changed))))
+(defn- send-changed! [pod client-opts change-map]
+  (pod/with-call-in pod
+    (adzerk.boot-reload.server/send-changed!
+     ~client-opts
+     ~change-map)))
 
 (defn- add-init!
-  [client-ns spec out-file cljs-edn-path]
-  (let [client-ns (symbol client-ns)]
-    (when (not= :nodejs (-> spec :compiler-options :target))
-      (util/info "Adding :require %s to %s...\n" client-ns cljs-edn-path)
-      (io/make-parents out-file)
-      (-> spec
-        (update-in [:require] conj client-ns)
-        pr-str
-        ((partial spit out-file))))))
+  [pod build-config old-spec out-file]
+  (when (not= :nodejs (-> old-spec :compiler-options :target))
+    (io/make-parents out-file)
+    (let [new-spec (pod/with-call-in pod
+                     (adzerk.boot-reload.server/add-cljs-edn-init
+                      ~build-config
+                      ~old-spec))]
+      (butil/info "Adding :require(s) to %s...\n" (.getName out-file))
+      (butil/dbug "%s\n" (butil/pp-str new-spec))
+      (->> new-spec
+           pr-str
+           (spit out-file)))))
 
-(defn- relevant-cljs-edn [fileset ids]
+(defn- relevant-cljs-edns [fileset ids]
   (let [relevant  (map #(str % ".cljs.edn") ids)
         f         (if ids
                     #(b/by-path relevant %)
                     #(b/by-ext [".cljs.edn"] %))]
     (-> fileset b/input-files f)))
+
+(defn cljs-opts-seq
+  "Return a sequence of compiler options given boot's .cljs.edn files on
+  the fileset."
+  [tmpfiles]
+  (mapv #(-> {}
+             (assoc :build-id (util/build-id (:path %)))
+             (merge (:adzerk.boot-cljs/opts %)))
+        tmpfiles))
+
+(defn make-build-config
+  "Return an ClojureScript build configuration map akin to leinengen and
+  figwheel:
+    https://github.com/bhauman/lein-figwheel#client-side-configuration-options
+
+  The id is last because it is the one more likely to vary."
+  [ws-url ws-host on-jsload client-opts build-id]
+  ;; See https://github.com/bhauman/lein-figwheel/blob/0283f6d79af630854c1d3071eeb6f0ff8fd41676/sidecar/src/figwheel_sidecar/schemas/config.clj#L602
+  (-> {:id build-id
+       :figwheel {:websocket-host ws-host
+                  :websocket-url ws-url}}
+      (assoc-in [:figwheel :on-jsload] on-jsload)
+      (update :figwheel merge client-opts)))
 
 (deftask reload
   "Live reload of page resources in browser via websocket.
@@ -103,41 +123,59 @@
 
   [b ids BUILD_IDS #{str} "Only inject reloading into these builds (= .cljs.edn files)"
    ;; Websocket Server
-   i ip ADDR         str  "The IP address for the websocket server to listen on. (optional)"
-   p port PORT       int  "The port the websocket server listens on. (optional)"
+   i ip      ADDR    str  "The IP address for the websocket server to listen on. (optional)"
+   p port    PORT    int  "The port the websocket server listens on. (optional)"
    _ ws-port PORT    int  "The port the websocket will connect to. (optional)"
    w ws-host WSADDR  str  "The websocket host clients connect to. Defaults to current host. (optional)"
    s secure          bool "Flag to indicate whether the client should connect via wss. Defaults to false."
-   ;; Other Configuration
-   j on-jsload SYM     sym "The callback to call when JS files are reloaded. (optional)"
-   _ asset-host HOST   str "The asset-host where to load files from. Defaults to host of opened page. (optional)"
-   a asset-path PATH   str "Sets the output directory for temporary files used during compilation. (optional)"
-   c cljs-asset-path PATH str "The actual asset path. This is added to the start of reloaded urls. (optional)"
-   o open-file COMMAND str "The command to run when warning or exception is clicked on HUD. Passed to format. (optional)"
-   v disable-hud      bool "Toggle to disable HUD. Defaults to false (visible)."
-   t target-path      VAL str "Target path to load files from, used WHEN serving files using file: protocol. (optional)"
-   _ only-by-re REGEXES [regex] "Vector of path regexes (for `boot.core/by-re`) to restrict reloads to only files within these paths (optional)."]
+   ;; Client Configuration
+   j on-jsload       SYM    sym  "The callbacks to call when JS files are reloaded. (optional)"
+   _ client-opts     OPTS   edn  "Options passed to the client directly (overrides the others)."
+   v disable-hud            bool "Toggle to disable HUD. Defaults to false (visible)."
+   o open-file       CMD    str  "The command to run when warning or exception is clicked on HUD. Passed to format. (optional)"
+   ;; Other Configuration - AR - (?)
+   _ asset-host      HOST   str  "The asset-host where to load files from. Defaults to host of opened page. (optional)"
+   a asset-path      PATH   str  "Sets the output directory for temporary files used during compilation. (optional)"
+   c cljs-asset-path PATH   str  "The actual asset path. This is added to the start of reloaded urls. (optional)"
+   t target-path     VAL    str  "Target path to load files from, used WHEN serving files using file: protocol. (optional)"
+   _ only-by-re      REGEX [regex] "Vector of path regexes (for `boot.core/by-re`) to restrict reloads to only files within these paths (optional)."]
 
-  (let [pod  (make-pod)
+  (let [ids  (or ids #{"main"})
+        pod  (make-pod)
         tmp  (tmp-dir!)
         prev-pre (atom nil)
         prev (atom nil)
         url  (start-server @pod {:ip ip :port port :ws-host ws-host
                                  :ws-port ws-port :secure? secure
-                                 :open-file open-file})]
+                                 :open-file open-file})
+        client-opts (assoc client-opts :project-id (-> (b/get-env) :directories string/join digest/md5))]
     (b/cleanup (pod/with-call-in @pod (adzerk.boot-reload.server/stop)))
     (fn [next-task]
       (fn [fileset]
+        ;; AR - TODO - pass the :open-file option down to figwheel and figure
+        ;; out how to handle it
         (pod/with-call-in @pod
           (adzerk.boot-reload.server/set-options {:open-file ~open-file}))
-        (doseq [f (relevant-cljs-edn (b/fileset-diff @prev-pre fileset) ids)]
-          (let [path     (tmp-path f)
-                spec     (-> f tmp-file slurp read-string)
-                client-ns (str "adzerk.boot-reload." (rutil/path->ns path))
-                out-file (io/file tmp path)]
-            (write-cljs! tmp client-ns url ws-host (get spec :on-jsload on-jsload) asset-host)
-            (add-init! client-ns spec out-file path)))
+
+        (let [cljs-edns (relevant-cljs-edns (b/fileset-diff @prev-pre fileset) ids)]
+          (when (and (empty? @prev-pre) (empty? cljs-edns))
+            (butil/warn "WARNING: No .cljs.edn file found on the fileset, boot-reload might misbehave.\n"))
+          (doseq [f cljs-edns]
+            (let [path     (tmp-path f)
+                  spec     (-> f tmp-file slurp read-string)
+                  build-config (make-build-config url
+                                                  ws-host
+                                                  (get spec :on-jsload on-jsload)
+                                                  client-opts
+                                                  (util/build-id path))]
+              (butil/dbug "Build config:\n%s\n" (butil/pp-str build-config))
+              ;; Writing the connection script to the tmp folder
+              (write-bootstrap-ns! @pod tmp build-config)
+              ;; Add the init files to the fileset
+              (add-init! @pod build-config spec (io/file tmp path)))))
         (reset! prev-pre fileset)
+
+        ;; Compile by triggering the chain of tasks
         (let [fileset (-> fileset (add-resource tmp) commit!)
               fileset (try
                         (next-task fileset)
@@ -147,27 +185,36 @@
                           (if (and (not disable-hud)
                                    (or (= :boot-cljs (:from (ex-data e)))
                                        (:adzerk.boot-reload/exception (ex-data e))))
-                            (send-visual! @pod {:exception (merge {:message (.getMessage e)}
-                                                                  (ex-data e))}))
+                            (send-visual! @pod client-opts {:exception (util/serialize-exception e)}))
                           (throw e)))]
-          (let [cljs-edn (relevant-cljs-edn fileset ids)
+          (let [cljs-edns (relevant-cljs-edns fileset ids)
                 ;; cljs uses specific key for now
                 ;; but any other file can contain warnings for boot-reload
-                warnings (concat (mapcat :adzerk.boot-cljs/warnings cljs-edn)
+                warnings (concat (mapcat :adzerk.boot-cljs/warnings cljs-edns)
                                  (mapcat :adzerk.boot-reload/warnings (b/input-files fileset)))
-                static-files (->> cljs-edn
+                cljs-opts-seq (cljs-opts-seq cljs-edns)
+                static-files (->> cljs-edns
                                   (map b/tmp-path)
                                   (map(fn [x] (string/replace x #"\.cljs\.edn$" ".js")))
                                   set)]
+            (butil/dbug "Compile opts:\n%s\n" (butil/pp-str cljs-opts-seq))
+            (when (> @butil/*verbosity* 2)
+              (butil/dbug "Warnings:\n%s\n" (butil/pp-str warnings))
+              (doseq [edn cljs-edns]
+                (butil/dbug "Meta on %s:\n%s\n" (:path edn) (butil/pp-str (select-keys edn [])))))
             (if-not disable-hud
-              (send-visual! @pod {:warnings warnings}))
-            ; Only send changed files when there are no warnings
-            ; As prev is updated only when changes are sent, changes are queued untill they can be sent
-            (when (empty? warnings)
-              (send-changed! @pod
-                             asset-path
-                             cljs-asset-path
-                             target-path
-                             (changed @prev fileset only-by-re static-files))
-              (reset! prev fileset))
+              (send-visual! @pod client-opts {:warnings warnings}))
+            ;; Only send changed files when there are no warnings
+            ;; As prev is updated only when changes are sent, changes are queued untill they can be sent
+            ;;
+            ;; AR - the above assumption changes when using figwheel's client,
+            ;; where the event order (unfortunately) matters.
+            (send-changed! @pod
+                           client-opts
+                           {:target-path target-path
+                            :asset-path asset-path
+                            :cljs-asset-path cljs-asset-path
+                            :cljs-opts-seq cljs-opts-seq
+                            :change-set (changed @prev fileset only-by-re static-files)})
+            (reset! prev fileset)
             fileset))))))
--- src/adzerk/boot_reload/server.clj
+++ src/adzerk/boot_reload/server.clj
@@ -3,7 +3,9 @@
    [clojure.java.io    :as io]
    [boot.util          :as util]
    [org.httpkit.server :as http]
-   [clojure.string     :as string])
+   [clojure.string     :as string]
+   [adzerk.boot-reload.figwheel :as figwheel]
+   [adzerk.boot-reload.messages :as msgs])
   (:import
    [java.io IOException]))
 
@@ -14,31 +16,76 @@
 (defn set-options [opts]
   (reset! options opts))
 
-(defn web-path
-  ([rel-path] (web-path {} rel-path))
-  ([opts rel-path]
-   ; windows fix, convert \ characters to / in rel-path
-   (let [rel-path (string/replace rel-path #"\\" "/")
-         {:keys [target-path asset-path cljs-asset-path]} opts]
-     {:canonical-path (.getCanonicalPath (io/file target-path rel-path))
-      :web-path (str
-                  cljs-asset-path "/"
-                  (string/replace rel-path
-                                  (re-pattern (str "^" (string/replace (or asset-path "") #"^/" "") "/"))
-                                  ""))})))
+(defn bootstrap-ns
+  "Computes the bootstrap namespace
 
-(defn send-visual! [messages]
-  (doseq [channel @clients]
-    (http/send! channel (pr-str (merge {:type :visual}
-                                       messages)))))
+  Return [ns-sym ns-path ns-content]."
+  [build-config]
+  [(figwheel/bootstrap-ns-name build-config)
+   (figwheel/bootstrap-ns-path build-config)
+   (->> build-config
+        figwheel/bootstrap-ns-content
+        (map util/pp-str)
+        (interpose "\n")
+        (apply str))])
+
+;; AR The cousin of:
+;; https://github.com/bhauman/lein-figwheel/blob/cc2d188ab041fc92551d3c4a8201729c47fe5846/sidecar/src/figwheel_sidecar/build_middleware/injection.clj#L171
+(defn- add-cljs-edn-init
+  "Add requires and other init stuff to the .cljs.edn spec
+
+  Return the new cljs.edn spec as data (not a string)."
+  [build-config cljs-spec]
+  (update cljs-spec :require #(->> (conj %
+                                         (-> build-config
+                                             figwheel/bootstrap-ns-name
+                                             symbol)
+                                         (when (get-in build-config [:figwheel :devcards])
+                                           'devcards.core))
+                                   (into #{})
+                                   (remove nil?)
+                                   vec)))
+
+;;;;;;;;;;;;;;;
+;; WEBSOCKET ;;
+;;;;;;;;;;;;;;;
 
 (defn send-changed!
-  ([changed] (send-changed! {} changed))
-  ([opts changed]
+  ([change-map] (send-changed! {} change-map))
+  ([opts change-map]
+   (when (> @boot.util/*verbosity* 2)
+     (util/dbug "Watch received:\n%s\n" (util/pp-str change-map)))
    (doseq [channel @clients]
-     (http/send! channel
-                 (pr-str {:type :reload
-                          :files (map #(web-path opts %) changed)})))))
+     (let [payloads (->> change-map
+                         (msgs/changed-messages opts)
+                         (mapv #(merge % (select-keys opts [:figwheel-version]))))]
+       (run! #(let [payload (util/pp-str %)]
+                (util/dbug "Sending:\n%s\n" payload)
+                (http/send! channel payload))
+             payloads)))))
+
+(defn send-visual!
+  "Send a visual notification to the connected clients.
+
+  A visual map is a map that has its key equal to the type (:warning,
+  exception, ...) of the notification and its value equal to a sequence
+  of payloads."
+  ([visual-map] (send-visual! {} visual-map))
+  ([opts visual-map]
+   (when (> @boot.util/*verbosity* 2)
+     (util/dbug "Watch received:\n%s\n" visual-map))
+   (doseq [channel @clients]
+     ;; AR - TODO handle multiple warnings and exceptions
+     (let [payload (->> visual-map
+                        (msgs/visual-message opts)
+                        (merge (select-keys opts [:figwheel-version])))]
+       (when-not (empty? payload)
+         (util/dbug "Sending:\n%s\n" payload)
+         (http/send! channel (util/pp-str payload)))))))
+
+;;;;;;;;;;;;;;;
+;; WEBSOCKET ;;
+;;;;;;;;;;;;;;;
 
 (defmulti handle-message (fn [channel message] (:type message)))
 
@@ -52,14 +99,26 @@
           (util/fail "There was a problem running open-file command: %s\n" cmd))))))
 
 (defn connect! [channel]
+  (util/dbug "Channel \"%s\" opened...\n" (str channel))
+
   (swap! clients conj channel)
+  (when (> @boot.util/*verbosity* 2)
+    (util/dbug "Connected clients %s\n" (mapv str @clients)))
+
   (http/on-close channel (fn [_] (swap! clients disj channel)))
-  (http/on-receive channel #(handle-message channel (read-string %))))
+  (http/on-receive channel (fn [data]
+                             (when (> @boot.util/*verbosity* 2)
+                               (util/dbug "Websocket received:\n%s\n" (util/pp-str data)))
+                             (handle-message channel (read-string data))))
+
+  (util/info "New websocket client connected!\n"))
 
 (defn handler [request]
   (if-not (:websocket? request)
     {:status 501 :body "Websocket connections only."}
-    (http/with-channel request channel (connect! channel))))
+    (do (when (> @boot.util/*verbosity* 2)
+          (util/dbug "Websocket received:\n%s\n" (util/pp-str request)))
+        (http/with-channel request channel (connect! channel)))))
 
 (defn start
   [{:keys [ip port] :as opts}]
