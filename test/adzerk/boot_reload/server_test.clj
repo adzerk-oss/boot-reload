d3a79c80c0e561e9ea6501336eb07a51ce2b2960
Juho Teperi <juho.teperi@iki.fi>
2016-12-21 01:53:42 +0200

    Fix web-path tests

--- test/adzerk/boot_reload/server_test.clj
+++ test/adzerk/boot_reload/server_test.clj
@@ -1,32 +1,45 @@
 (ns adzerk.boot-reload.server-test
   (:require [clojure.test :refer :all]
-            [adzerk.boot-reload.server :refer :all]))
+            [adzerk.boot-reload.server :refer :all]
+            [clojure.java.io :as io]))
 
 (deftest web-path-test
   (testing "basic"
-    (is (= "/js/out/saapas/core.js"
-           (web-path {:protocol "http:" :target-path "target"} "js/out/saapas/core.js"))))
+    (is (= {:web-path "/js/out/saapas/core.js"
+            :canonical-path (.getCanonicalPath (io/file "target" "js" "out" "saapas" "core.js"))}
+           (web-path {:target-path "target"} "js/out/saapas/core.js"))))
 
   (testing "asset-path"
-    (is (= "/js/out/saapas/core.js"
-           (web-path {:protocol "http:" :asset-path "public"} "public/js/out/saapas/core.js")))
+    (is (= {:web-path "/js/out/saapas/core.js"
+            :canonical-path (.getCanonicalPath (io/file "target" "public" "js" "out" "saapas" "core.js"))}
+           (web-path {:asset-path "public"
+                      :target-path "target"}
+                     "public/js/out/saapas/core.js")))
 
-    (is (= "/js/out/saapas/core.js"
-           (web-path {:protocol "http:" :asset-path "/public"} "public/js/out/saapas/core.js")))
+    (is (= {:web-path "/js/out/saapas/core.js"
+            :canonical-path (.getCanonicalPath (io/file "target" "public" "js" "out" "saapas" "core.js"))}
+           (web-path {:asset-path "/public"
+                      :target-path "target"}
+                     "public/js/out/saapas/core.js")))
 
-    (is (= "/public/js/out/saapas/core.js"
-           (web-path {:protocol "http:" :asset-path "foobar"} "public/js/out/saapas/core.js"))))
+    (is (= {:web-path "/public/js/out/saapas/core.js"
+            :canonical-path (.getCanonicalPath (io/file "target" "public" "js" "out" "saapas" "core.js"))}
+           (web-path {:asset-path "foobar"
+                      :target-path "target"}
+                     "public/js/out/saapas/core.js"))))
 
   (testing "cljs-asset-path"
-    (is (= "js/saapas.out/saapas/core.js"
-           (web-path {:protocol "http:"
-                      :asset-path "resources/public/js/saapas.out"
-                      :cljs-asset-path "js/saapas.out"}
+    (is (= {:web-path "js/saapas.out/saapas/core.js"
+            :canonical-path (.getCanonicalPath (io/file "target" "resources" "public" "js" "saapas.out" "saapas" "core.js"))}
+           (web-path {:asset-path "resources/public/js/saapas.out"
+                      :cljs-asset-path "js/saapas.out"
+                      :target-path "target"}
                      "resources/public/js/saapas.out/saapas/core.js"))))
 
   (testing "windows style paths"
-    (is (= "js/saapas.out/saapas/core.js"
-           (web-path {:protocol "http:"
-                      :asset-path "resources/public/js/saapas.out"
-                      :cljs-asset-path "js/saapas.out"}
+    (is (= {:web-path "js/saapas.out/saapas/core.js"
+            :canonical-path (.getCanonicalPath (io/file "target" "resources" "public" "js" "saapas.out" "saapas" "core.js"))}
+           (web-path {:asset-path "resources/public/js/saapas.out"
+                      :cljs-asset-path "js/saapas.out"
+                      :target-path "target"}
                      "resources\\public\\js\\saapas.out\\saapas\\core.js")))))
