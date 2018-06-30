0005bc7018db419dfeeeccb58a6d389c32a4a2b2
Juho Teperi <juho.teperi@iki.fi>
2018-06-30 21:00:38 +0300

    Fix test script

--- test.sh
+++ test.sh
@@ -3,16 +3,16 @@
 set -e
 
 echo "1.9.0-alpha17"
-BOOT_CLOJURE_VERSION=1.9.0-alpha17 boot run-tests
+BOOT_CLOJURE_VERSION=1.9.0 boot test
 
 echo
 echo "1.8.0"
-BOOT_CLOJURE_VERSION=1.8.0 boot run-tests
+BOOT_CLOJURE_VERSION=1.8.0 boot test
 
 echo
 echo "1.7.0"
-BOOT_CLOJURE_VERSION=1.7.0 boot run-tests
+BOOT_CLOJURE_VERSION=1.7.0 boot test
 
 echo
 echo "1.6.0"
-BOOT_CLOJURE_VERSION=1.6.0 boot run-tests
+BOOT_CLOJURE_VERSION=1.6.0 boot test
